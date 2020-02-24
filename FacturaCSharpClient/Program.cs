d3a078e0f1e912d0adf2a9a7c3ad5e4d594c1608
Stefan Tschiharz <stefan.tschiharz@vfm-gruppe.de>
2020-02-24 10:11:33 +0100

    Initialer Commit für FacturaCSharpClient

---
+++ FacturaCSharpClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace FacturaCSharpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FacturaTestService.ServerService fs = new FacturaCSharpClient.FacturaTestService.ServerService();
+            var response = fs.login("******", "************");
+            fs.setKunde(null, true);
+        }
+    }
+}
